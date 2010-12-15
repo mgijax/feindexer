dbb30f2e5e9b4f84f082b7f74f4c01be70d78da1
Matt Hall <Matt.Hall@jax.org>
2010-12-15 15:36:25 +0000

    Several changes, one of which removed duplicate authors.

--- src/org/jax/mgi/indexer/AuthorsAutoCompleteIndexerSQL.java
+++ src/org/jax/mgi/indexer/AuthorsAutoCompleteIndexerSQL.java
@@ -103,7 +103,7 @@ public class AuthorsAutoCompleteIndexerSQL extends Indexer {
                 
                 // Parse out the first 4 tokens of the author field, and make display tokens for them.
                 
-                String [] temp = rs_overall.getString("author").split("[\\W&&[^']]");
+                String [] temp = rs_overall.getString("author").split("[\\W-&&[^']]");
                 
                 //doc = new SolrInputDocument();
                 
@@ -112,7 +112,7 @@ public class AuthorsAutoCompleteIndexerSQL extends Indexer {
                 if (temp.length > 1) {
                     for (int i = temp.length - 1; i>= 0; i--) {
                             String tempString = "";
-                            for (int j = 0; j < temp.length && j < 4; j++) {
+                            for (int j = 0; j < temp.length - 1 && j < 4; j++) {
                                 //System.out.println("getting here");
                                 if (j == 0) {
                                     tempString += temp[j];
@@ -122,61 +122,18 @@ public class AuthorsAutoCompleteIndexerSQL extends Indexer {
                                 }
                                 
                                 if (tempString != "") {
-                                    //System.out.println("authorSort: " + tempString);
                                     doc.addField(IndexConstants.REF_AUTHOR_SORT, tempString);
-                                    //System.out.println("testString: " + rs_overall.getString("author"));
-                                    //doc.addField("testString", rs_overall.getString("author"));
                                     parseAuthor(doc, tempString);
                                     docs.add(doc);
-                                    //System.out.println("==================================");
                                     doc = new SolrInputDocument();
                                 }
                             }
                     }
                 }
                 
-                //String [] temp = rs_overall.getString("author").split("\\W");
-                
-                // Add all possible prefix combinations of this author into the index.
-                
-/*                if (temp.length > 1) {
-                    for (int i = 1; i< temp.length; i++) {
-                            String tempString = "";
-                            for (int j = i; j < temp.length; j++) {
-                                if (j == 1) {
-                                    tempString += temp[j];
-                                }
-                                else {
-                                    tempString += " " + temp[j];
-                                }
-                            }
-                        doc.addField(IndexConstants.REF_AUTHOR, tempString);
-                    }
-                }*/
-                
-                // Since we cannot sort by a multi valued field we add the author back
-                // in in an unmodified form into the author_sort column.  This column
-                // also doubles as the display field. 
-                
-                //doc.addField(IndexConstants.REF_AUTHOR_SORT, rs_overall.getString("author"));
-                //docs.add(doc);
-/*                String [] foo = rs_overall.getString("author").split(" ");
-                authorSet.add(foo[0]);*/
+
             }
             
-            // Finally we need to add in a special author that is only the base part of
-            // each author string.  For example this means we can now search for
-            // all of the eppigs at once.
-            
-/*            for (String author: authorSet) {
-                SolrInputDocument doc = new SolrInputDocument();
-                doc.addField(IndexConstants.REF_AUTHOR, author);
-                doc.addField(IndexConstants.REF_AUTHOR_SORT, author);
-                docs.add(doc);
-            }
-            
-            // Add all of the documents to the index.
-            */
             server.add(docs);
             server.commit();
             
