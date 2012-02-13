fad9e9bcf93200bca180fc58d30b3e741b0fd437
Jonathan Beal <Jon.Beal@jax.org>
2012-02-13 21:14:16 +0000

    gutted this class to make data more granular

--- src/org/jax/mgi/searchInfo/MarkerSearchInfo.java
+++ src/org/jax/mgi/searchInfo/MarkerSearchInfo.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 /**
  * MarkerSearchInfo
- * @author mhall
  * This object represents all of the search information needed about a marker for a specific index.
  * 
  * It is expected that the index that uses this object will use all of the information that it contains.
@@ -14,39 +13,83 @@ import java.util.List;
 
 public class MarkerSearchInfo {
 	
-	private List<String> nomen = new ArrayList<String>();
 	private String bySymbol;
+	private String symbol;
+	private String name;
+	private List<String> synonyms = new ArrayList<String>();
 	
-	public void addNomen(String word) {
-		nomen.add(word);
+	/* setters */
+
+	public void addSynonym (String synonym) {
+		if (synonym != null) {
+			this.synonyms.add (synonym);
+		}
+	}
+
+	public void setBySymbol(String bySymbol) {
+		this.bySymbol = bySymbol;
+	}
+
+	public void setSymbol (String symbol) {
+		this.symbol = symbol;
+	}
+
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	/* getters */
+
+	public String getSymbol() {
+		return this.symbol;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public List<String> getSynonyms() {
+		return this.synonyms;
 	}
 
 	public String getBySymbol() {
 		return bySymbol;
 	}
 
+	/* accessories */
+
 	public List<String> getNomen() {
-		return nomen;
+		List<String> allNomen = new ArrayList();
+
+		if (this.symbol != null) { allNomen.add (symbol); }
+		if (this.name != null) { allNomen.add (name); }
+		allNomen.addAll (this.synonyms);
+
+		return allNomen;
 	}
 	
-	public void setBySymbol(String bySymbol) {
-		this.bySymbol = bySymbol;
-	}
-
-	public void setNomen(List<String> nomen) {
-		this.nomen = nomen;
-	}
-
 	@Override
 	public String toString() {
-		String outString = "";
-		for (String n: nomen) {
-			outString += "\n" + n;
+		StringBuffer sb = new StringBuffer();
+
+		if (this.symbol != null) { sb.append ("Symbol: " +
+			this.symbol + "\n");
+		}
+		if (this.name != null) { sb.append ("Name: " +
+			this.name + "\n");
+		}
+		if (this.synonyms.size() > 0) {
+			sb.append ("Synonyms: ");
+
+			for (String s : synonyms) {
+				sb.append (s);
+				sb.append ("; ");
+			}
+
+			sb.append ("\n");
 		}
 		
-		return outString;
-	};
-		
-	
+		return sb.toString();
+	}
 	
 }
