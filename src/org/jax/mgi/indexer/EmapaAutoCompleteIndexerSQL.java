738e3b108081bdb9dae91a9321864b3ba4e9107b
Kevin Stone <Kevin.Stone@jax.org>
2014-03-08 18:01:54 +0000

    Adding has cre flag

--- src/org/jax/mgi/indexer/EmapaAutoCompleteIndexerSQL.java
+++ src/org/jax/mgi/indexer/EmapaAutoCompleteIndexerSQL.java
@@ -22,8 +22,6 @@ import org.jax.mgi.shr.fe.sort.SmartAlphaComparator;
  * StrucureAutoCompleteIndexerSQL, as we still need that one to support the
  * Cre/recombinase QF.
  *
- * Notably, this one does not support flagging structures as having Cre in
- * the index.
  */
 
 public class EmapaAutoCompleteIndexerSQL extends Indexer 
@@ -39,157 +37,149 @@ public class EmapaAutoCompleteIndexerSQL extends Indexer
     	
         logger.info("Getting all distinct structures & synonyms");
         String query = "WITH anatomy_synonyms as "+
-	    "(select distinct t.term structure, " +
-	    "  ts.synonym, "+
-
-/* disabled until we want to use the picklist for actually picking a specific
- * term to search by ID, rather than a set of words
- *	    "  t.primary_id, " +
- *	    "  e.start_stage, " +
- *	    "  e.end_stage, " +
- */
-
-	    "  false as has_cre " +
-	    "from term t " +
-	    "inner join term_emap e on t.term_key = e.term_key "+
-	    "left outer join term_synonym ts on t.term_key = ts.term_key "+
-	    "where t.vocab_name='EMAPA') "+
-
-	    "select distinct a1.structure, " +
-	    "  a1.synonym, " +
-	    "  a1.has_cre, " +
-
-/* disabled until we want to use the picklist for actually picking a specific
- * term to search by ID, rather than a set of words
- *	    "  a1.primary_id, " +
- *	    "  a1.start_stage, " +
- *	    "  a1.end_stage, " +
- */
-
-	    "  case when (exists (select 1 from anatomy_synonyms a2 " +
-	    "    where a2.structure=a1.synonym)) "+
+        	"(select distinct t.term structure, ts.synonym, "+
+			/* disabled until we want to use the picklist for actually picking a specific
+			 * term to search by ID, rather than a set of words
+			 *	    "  t.primary_id, " +
+			 *	    "  e.start_stage, " +
+			 *	    "  e.end_stage, " +
+			 */
+		    "  false as has_cre " +
+		    "case when (exists (select 1 from recombinase_assay_result rar where rar.structure=t.term)) "+
+			"then true else false end as has_cre "+
+			 "from term t left outer join term_synonym ts "+
+            					"on t.term_key=ts.term_key "+
+					"where t.vocab_name='EMAPA') "+
+		    "from term t join term_emap e on t.term_key = e.term_key left outer join " +
+		    "term_synonym ts on t.term_key = ts.term_key "+
+		    "where t.vocab_name='EMAPA') "+
+	    "select distinct a1.structure, a1.synonym, a1.has_cre, " +
+		/* disabled until we want to use the picklist for actually picking a specific
+		 * term to search by ID, rather than a set of words
+		 *	    "  a1.primary_id, " +
+		 *	    "  a1.start_stage, " +
+		 *	    "  a1.end_stage, " +
+		 */
+	    "  case when (exists (select 1 from anatomy_synonyms a2 where a2.structure=a1.synonym)) "+
 	    "    then false else true end as is_strict_synonym "+
 	    "from anatomy_synonyms a1 "+
 	    "order by a1.structure ";
 
-        ResultSet rs_overall = ex.executeProto(query);
+        ResultSet rs = ex.executeProto(query);
             
         Collection<SolrInputDocument> docs =
 	    new ArrayList<SolrInputDocument>();
 
-	// need to gather the terms and synonyms that will appear in the pick
-	// list, so we can compute an ordering for them
-
+		// need to gather the terms and synonyms that will appear in the pick
+		// list, so we can compute an ordering for them
         logger.info("calculating sorts");
-        while(rs_overall.next()) {
-	    String term = rs_overall.getString("structure");
-	    String synonym = rs_overall.getString("synonym");
-
-/* disabled until we want to use the picklist for actually picking a specific
- * term to search by ID, rather than a set of words
- *	    String startStage = rs_overall.getString("start_stage");
- *	    String accID = rs_overall.getString("primary_id");
- *
- *	    String tag = "-" + startStage + "-" + accID;
- *
- *	    // need to figure in the startStage, to account for terms which
- *	    // appear as separate terms in different stage ranges
- *
- *	    termsToSort.add(term + tag);
- *	    termsToSort.add(synonym + tag);
- */
-
- 	    termsToSort.add(term);
- 	    termsToSort.add(synonym);
-	}
+        while(rs.next()) 
+        {
+		    String term = rs.getString("structure");
+		    String synonym = rs.getString("synonym");
+			/* disabled until we want to use the picklist for actually picking a specific
+			 * term to search by ID, rather than a set of words
+			 *	    String startStage = rs_overall.getString("start_stage");
+			 *	    String accID = rs_overall.getString("primary_id");
+			 *
+			 *	    String tag = "-" + startStage + "-" + accID;
+			 *
+			 *	    // need to figure in the startStage, to account for terms which
+			 *	    // appear as separate terms in different stage ranges
+			 *
+			 *	    termsToSort.add(term + tag);
+			 *	    termsToSort.add(synonym + tag);
+			 */
+		 	termsToSort.add(term);
+	 	    termsToSort.add(synonym);
+		}
 
         // sort the terms and assign a sort value for each in termSort
-
         Collections.sort(termsToSort,new SmartAlphaComparator());
 
-        for (int i=0; i < termsToSort.size(); i++) {
-	    termSort.put(termsToSort.get(i), i);
-	}
+        for (int i=0; i < termsToSort.size(); i++) 
+        {
+        	termSort.put(termsToSort.get(i), i);
+        }
             
         logger.info("Creating the documents");
+        rs = ex.executeProto(query);
             
-        rs_overall = ex.executeProto(query);
-            
-        while (rs_overall.next()) {
+        while (rs.next()) 
+        {
             // add the synonym structure combo
-	    String structure = rs_overall.getString("structure");
-	    String synonym = rs_overall.getString("synonym");
-	    Boolean hasCre = rs_overall.getBoolean("has_cre");
+		    String structure = rs.getString("structure");
+		    String synonym = rs.getString("synonym");
+		    Boolean hasCre = rs.getBoolean("has_cre");
+	
+			/* disabled until we want to use the picklist for actually picking a specific
+			 * term to search by ID, rather than a set of words
+			 *	    String startStage = rs.getString("start_stage");
+			 *	    String endStage = rs.getString("end_stage");
+			 *	    String accID = rs.getString("primary_id");
+			 *
+			 *          // structure_key is merely a unique id so that Solr is happy,
+			 *	    // because structures and synonyms can repeat. 
+			 *
+			 * 	    String tag = "-" + startStage + "-" + accID;
+			 *          String structure_key = structure+"-"+synonym + "-" + tag;
+			 */
+		    String structure_key = structure + "-" + synonym;
 
-/* disabled until we want to use the picklist for actually picking a specific
- * term to search by ID, rather than a set of words
- *	    String startStage = rs_overall.getString("start_stage");
- *	    String endStage = rs_overall.getString("end_stage");
- *	    String accID = rs_overall.getString("primary_id");
- *
- *          // structure_key is merely a unique id so that Solr is happy,
- *	    // because structures and synonyms can repeat. 
- *
- * 	    String tag = "-" + startStage + "-" + accID;
- *          String structure_key = structure+"-"+synonym + "-" + tag;
- */
-	    String structure_key = structure + "-" + synonym;
-
-	    // add an entry for the synonym, if it is defined
-
-            if (synonym != null && !synonym.equals("") &&
-		!uniqueIds.contains(structure_key)) {
-
-		// strict synonym means that this term only exists as a synonym
-                Boolean isStrictSynonym = rs_overall.getBoolean("is_strict_synonym");
+		    // add an entry for the synonym, if it is defined
+            if (synonym != null && !synonym.equals("") && !uniqueIds.contains(structure_key)) 
+            {
+            	// strict synonym means that this term only exists as a synonym
+                Boolean isStrictSynonym = rs.getBoolean("is_strict_synonym");
                     
                 uniqueIds.add(structure_key);
-	        SolrInputDocument doc = new SolrInputDocument();
-	        doc.addField(IndexConstants.STRUCTUREAC_STRUCTURE, structure);
-	        doc.addField(IndexConstants.STRUCTUREAC_SYNONYM, synonym);
+                SolrInputDocument doc = new SolrInputDocument();
+                doc.addField(IndexConstants.STRUCTUREAC_STRUCTURE, structure);
+                doc.addField(IndexConstants.STRUCTUREAC_SYNONYM, synonym);
 
-/* disabled until we want to use the picklist for actually picking a specific
- * term to search by ID, rather than a set of words
- *		doc.addField(IndexConstants.GXD_START_STAGE, startStage);
- *		doc.addField(IndexConstants.GXD_END_STAGE, endStage);
- *		doc.addField(IndexConstants.ACC_ID, accID);
- *	        doc.addField(IndexConstants.STRUCTUREAC_BY_SYNONYM, termSort.get(synonym + tag));
- */
+				/* disabled until we want to use the picklist for actually picking a specific
+				 * term to search by ID, rather than a set of words
+				 *		doc.addField(IndexConstants.GXD_START_STAGE, startStage);
+				 *		doc.addField(IndexConstants.GXD_END_STAGE, endStage);
+				 *		doc.addField(IndexConstants.ACC_ID, accID);
+				 *	        doc.addField(IndexConstants.STRUCTUREAC_BY_SYNONYM, termSort.get(synonym + tag));
+				 */
 
-	        doc.addField(IndexConstants.STRUCTUREAC_BY_SYNONYM, termSort.get(synonym));
-	        doc.addField(IndexConstants.STRUCTUREAC_KEY,structure_key);
-	        doc.addField(IndexConstants.STRUCTUREAC_IS_STRICT_SYNONYM, isStrictSynonym);
-	        doc.addField(IndexConstants.STRUCTUREAC_HAS_CRE,hasCre);
-	        docs.add(doc);
+		        doc.addField(IndexConstants.STRUCTUREAC_BY_SYNONYM, termSort.get(synonym));
+		        doc.addField(IndexConstants.STRUCTUREAC_KEY,structure_key);
+		        doc.addField(IndexConstants.STRUCTUREAC_IS_STRICT_SYNONYM, isStrictSynonym);
+		        doc.addField(IndexConstants.STRUCTUREAC_HAS_CRE,hasCre);
+		        docs.add(doc);
             }
 
             // make sure that the base structure gets included as a "synonym"
 
-/* disabled until we want to use the picklist for actually picking a specific
- * term to search by ID, rather than a set of words
- *           structure_key = structure+"-"+structure + "-" + tag;
- */
+			/* disabled until we want to use the picklist for actually picking a specific
+			 * term to search by ID, rather than a set of words
+			 *           structure_key = structure+"-"+structure + "-" + tag;
+			 */
 
             structure_key = structure + "-" + structure;
-            if (!uniqueIds.contains(structure_key)) {
-		uniqueIds.add(structure_key);
-		SolrInputDocument doc = new SolrInputDocument();
-		doc.addField(IndexConstants.STRUCTUREAC_STRUCTURE, structure);
-		doc.addField(IndexConstants.STRUCTUREAC_SYNONYM, structure);
+            if (!uniqueIds.contains(structure_key)) 
+            {
+				uniqueIds.add(structure_key);
+				SolrInputDocument doc = new SolrInputDocument();
+				doc.addField(IndexConstants.STRUCTUREAC_STRUCTURE, structure);
+				doc.addField(IndexConstants.STRUCTUREAC_SYNONYM, structure);
 
-/* disabled until we want to use the picklist for actually picking a specific
- * term to search by ID, rather than a set of words
- *		doc.addField(IndexConstants.GXD_START_STAGE, startStage);
- *		doc.addField(IndexConstants.GXD_END_STAGE, endStage);
- *		doc.addField(IndexConstants.ACC_ID, accID);
- *		doc.addField(IndexConstants.STRUCTUREAC_BY_SYNONYM, termSort.get(structure + tag));
- */
+				/* disabled until we want to use the picklist for actually picking a specific
+				 * term to search by ID, rather than a set of words
+				 *		doc.addField(IndexConstants.GXD_START_STAGE, startStage);
+				 *		doc.addField(IndexConstants.GXD_END_STAGE, endStage);
+				 *		doc.addField(IndexConstants.ACC_ID, accID);
+				 *		doc.addField(IndexConstants.STRUCTUREAC_BY_SYNONYM, termSort.get(structure + tag));
+				 */
 
-		doc.addField(IndexConstants.STRUCTUREAC_BY_SYNONYM, termSort.get(structure));
-		doc.addField(IndexConstants.STRUCTUREAC_KEY,structure_key);
-		doc.addField(IndexConstants.STRUCTUREAC_IS_STRICT_SYNONYM, false);
-		doc.addField(IndexConstants.STRUCTUREAC_HAS_CRE,hasCre);
-		docs.add(doc);
+				doc.addField(IndexConstants.STRUCTUREAC_BY_SYNONYM, termSort.get(structure));
+				doc.addField(IndexConstants.STRUCTUREAC_KEY,structure_key);
+				doc.addField(IndexConstants.STRUCTUREAC_IS_STRICT_SYNONYM, false);
+				doc.addField(IndexConstants.STRUCTUREAC_HAS_CRE,hasCre);
+				docs.add(doc);
             }
         } // end while loop
             
