7838932d1770e2fef41d40770b38f88a8498bd82
Kevin Stone <Kevin.Stone@jax.org>
2014-03-20 19:40:44 +0000

    removing debugging

--- src/org/jax/mgi/indexer/MarkerIndexerSQL.java
+++ src/org/jax/mgi/indexer/MarkerIndexerSQL.java
@@ -154,10 +154,10 @@ public class MarkerIndexerSQL extends Indexer
             this.addAllFromLookup(doc,IndexConstants.MRK_TERM_ID,mrkKey,termToMarkersID);
             
 
-            logger.info("marker="+mrkKey);
+            //logger.info("marker="+mrkKey);
             if(termToMarkers.containsKey(mrkKey))
             {
-            	logger.info("has terms");
+            	//logger.info("has terms");
             	for(MarkerTerm mt : termToMarkers.get(mrkKey))
             	{
             		String field = "goTerm";
@@ -166,7 +166,7 @@ public class MarkerIndexerSQL extends Indexer
             		else if(GO_COMPONENT.equals(mt.vocab)) field = "goComponentTerm";
             		else if(INTERPRO_VOCAB.equals(mt.vocab)) field = "interProTerm";
             		
-            		logger.info("field="+field+",mtvocab="+mt.vocab+",term="+mt.term);
+            		//logger.info("field="+field+",mtvocab="+mt.vocab+",term="+mt.term);
             		doc.addField(field,mt.term);
             	}
             }
