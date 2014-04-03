f7482e32c6fc77c4d2fa777392a52ed019e2c1ef
Kevin Stone <Kevin.Stone@jax.org>
2014-04-03 20:28:29 +0000

    adjusting values

--- src/org/jax/mgi/shr/SolrUtils.java
+++ src/org/jax/mgi/shr/SolrUtils.java
@@ -22,7 +22,7 @@ public class SolrUtils {
     	if(fieldList.contains(field)) 
     	{
     		int idx = fieldList.indexOf(field);
-    		return (float) (maxBoost * Math.pow(0.2,idx));
+    		return (float) (maxBoost * Math.pow(0.05,idx));
     	}
     	return (float) 0;
     }
