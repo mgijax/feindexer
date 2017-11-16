2565d73d8c9a041960b7ba2b2d7e450e0b35e0f3
Jonathan Beal <jsb@jax.org>
2017-11-16 07:37:09 -0500

    large refactoring to switch from one record per ancestor to one record per annotation (with a multi-valued field for ancestors), plus some cleanup

--- src/org/jax/mgi/indexer/MPAnnotationIndexerSQL.java
+++ src/org/jax/mgi/indexer/MPAnnotationIndexerSQL.java
@@ -1,6 +1,7 @@
 package org.jax.mgi.indexer;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -16,51 +17,21 @@ import org.jax.mgi.shr.fe.IndexConstants;
 
 public class MPAnnotationIndexerSQL extends Indexer {
 
+	private String vocabName = "Mammalian Phenotype";
+	private	String annotationType = "Mammalian Phenotype/Genotype";
+
 	public MPAnnotationIndexerSQL () {
 		super("mpAnnotation");
 	}
 
-	public void index() throws Exception
-	{
-		String annotationType = "Mammalian Phenotype/Genotype";
-
-		// get highest annotation key for genotype annotations
-
+	// get highest annotation key for MP/genotype annotations
+	private Integer getMaxAnnotationKey() throws SQLException {
 		ResultSet rs_tmp = ex.executeProto("select max(annotation_key) as maxAnnotKey from annotation where annotation_type = '" + annotationType + "'");
 		rs_tmp.next();
-		Integer maxAnnotKey = rs_tmp.getInt("maxAnnotKey");
-
-		String tmpAnnotations = "select a.annotation_key, "
-				+ "  ga.genotype_key, "
-				+ "  a.term_id as search_term_id, "
-				+ "  a.term_id, "
-				+ "  a.qualifier "
-				+ "into temp tmp_mp_annotations "
-				+ "from annotation a, "
-				+ "  genotype_to_annotation ga, "
-				+ "  genotype g "
-				+ "where a.annotation_type = '" + annotationType + "' "
-				+ "  and ga.genotype_key = g.genotype_key "
-				+ "  and g.combination_1 is not null "
-				+ "  and a.annotation_key = ga.annotation_key "
-				+ "union "
-				+ "select distinct a.annotation_key, "
-				+ "  ga.genotype_key, "
-				+ "  ta.ancestor_primary_id as search_term_id, "
-				+ "  a.term_id, "
-				+ "  a.qualifier "
-				+ "from term t, term_ancestor ta, annotation a, "
-				+ "  genotype_to_annotation ga, genotype g "
-				+ "where a.annotation_type = '" + annotationType + "' "
-				+ "  and a.annotation_key = ga.annotation_key "
-				+ "  and ga.genotype_key = g.genotype_key "
-				+ "  and g.combination_1 is not null "
-				+ "  and a.term_id = t.primary_id "
-				+ "  and t.term_key = ta.term_key ";
-		
-		ex.executeVoid(tmpAnnotations);
-		logger.info("done creating temp table tmp_mp_annotations");
-
+		return rs_tmp.getInt("maxAnnotKey");
+	}
+	
+	public HashMap<String, HashSet<String>> getGenotypeToMarkersMap() {
 		// Now for each annotation, we need to find the markers associated
 		// with the alleles in the genotype (so we can match based on MP
 		// term and marker when linking from the batch query).  There are
@@ -92,14 +63,14 @@ public class MPAnnotationIndexerSQL extends Indexer {
 				+ "  and a.annotation_type = '" + annotationType + "' "
 				+ "  and a.annotation_key = ga.annotation_key "
 				+ "  and ag.allele_key = ma.allele_key "
-				+ "  and ma.relationship_category in ('mutation_involves', "
-				+ "    'expresses_component')"; 
+				+ "  and ma.relationship_category in ('mutation_involves', 'expresses_component')"; 
 
-		HashMap<String, HashSet<String>> genotypeToMarkers = makeHash(
-				genotypeMarkers, "genotype_key", "marker_key");
-		logger.info("Found markers for " + genotypeToMarkers.size()
-				+ " genotypes");
-
+		HashMap<String, HashSet<String>> genotypeToMarkers = makeHash(genotypeMarkers, "genotype_key", "marker_key");
+		logger.info("Found markers for " + genotypeToMarkers.size() + " genotypes");
+		return genotypeToMarkers;
+	}
+	
+	public HashMap<String, HashSet<String>> getAnnotationToRefsMap() {
 		// And each annotation will have one or more references as
 		// supporting evidence.  We need the J: numbers for each of them.
 
@@ -111,33 +82,15 @@ public class MPAnnotationIndexerSQL extends Indexer {
 				+ "  and a.annotation_type = '" + annotationType + "' "
 				+ "order by 1, 3";
 
-		HashMap<String, HashSet<String>> annotationToRefs = makeHash(
-				references, "annotation_key", "jnum_id");
-		logger.info("Found references for " + annotationToRefs.size()
-				+ " annotations");
+		HashMap<String, HashSet<String>> annotationToRefs = makeHash(references, "annotation_key", "jnum_id");
+		logger.info("Found references for " + annotationToRefs.size() + " annotations");
+		return annotationToRefs; 
+	}
 
-		// These should be able to be gathered in the mainQuery, but it
-		// hangs inexplicably when run in parallel with other gatherers.
-		// So, we're trying to work around that here by pulling out
-		// pieces of the query.
-
-		String termQuery = "select annotation_key, term, term_id "
-				+ "from annotation "
-				+ "where annotation_type = '" + annotationType + "' ";
-
-		HashMap<String, HashSet<String>> annotationToTerm = makeHash(
-				termQuery, "annotation_key", "term");
-		logger.info("Found terms for " + annotationToTerm.size()
-				+ " annotations");
-
-		HashMap<String, HashSet<String>> annotationToTermID = makeHash(
-				termQuery, "annotation_key", "term_id");
-		logger.info("Found term IDs for " + annotationToTermID.size()
-				+ " annotations");
-
-		String genotypeQuery = "select a.annotation_key, "
-				+ "  g.background_strain, "
-				+ "  g.combination_1 "
+	// dataType should be either 'background_strain' or 'combination_1', depending on what you want
+	public HashMap<String, HashSet<String>> getGenotypeInfoMap(String dataType) {
+		String genotypeQuery = "select distinct g.genotype_key, "
+				+ "  g." + dataType + " "
 				+ "from annotation a, "
 				+ "  genotype_to_annotation ga, "
 				+ "  genotype g "
@@ -145,16 +98,23 @@ public class MPAnnotationIndexerSQL extends Indexer {
 				+ "  and ga.annotation_key = a.annotation_key"
 				+ "  and a.annotation_type = '" + annotationType + "' ";
 
-		HashMap<String, HashSet<String>> annotationToStrain = makeHash(
-				genotypeQuery, "annotation_key", "background_strain");
-		logger.info("Found strains for " + annotationToStrain.size()
-				+ " annotations");
+		HashMap<String, HashSet<String>> genotypeToData = makeHash(genotypeQuery, "genotype_key", dataType);
+		logger.info("Found " + dataType + " for " + genotypeToData.size() + " genotypes");
+		return genotypeToData;
+	}
 
-		HashMap<String, HashSet<String>> annotationToAlleles = makeHash(
-				genotypeQuery, "annotation_key", "combination_1");
-		logger.info("Found alleles for " + annotationToAlleles.size()
-				+ " annotations");
+	// dataType should either be 'term' or 'primary_id', depending on what you want
+	public HashMap<String, HashSet<String>> getTermDataMap (String dataType) {
+		String cmd = "select term_key, " + dataType + " "
+			+ "from term "
+			+ "where vocab_name = '" + vocabName + "'";
 
+		HashMap<String, HashSet<String>> termKeyToData = makeHash(cmd, "term_key", dataType);
+		logger.info("Found " + dataType + " for " + termKeyToData.size() + " terms");
+		return termKeyToData;
+	}
+
+	public HashMap<String, HashSet<String>> getAnnotationToSeqNumMap() {
 		String seqnumQuery = "select a.annotation_key, "
 				+ "  s.by_object_dag_term "
 				+ "from annotation a, "
@@ -162,11 +122,48 @@ public class MPAnnotationIndexerSQL extends Indexer {
 				+ "where a.annotation_key = s.annotation_key "
 				+ "  and a.annotation_type = '" + annotationType + "' ";
 
-		HashMap<String, HashSet<String>> annotationToSeqNum = makeHash(
-				seqnumQuery, "annotation_key", "by_object_dag_term");
-		logger.info("Found sequence numbers for "
-				+ annotationToSeqNum.size() + " annotations");
+		HashMap<String, HashSet<String>> annotationToSeqNum = makeHash(seqnumQuery, "annotation_key", "by_object_dag_term");
+		logger.info("Found sequence numbers for " + annotationToSeqNum.size() + " annotations");
+		return annotationToSeqNum;
+	}
 
+	public HashMap<String, HashSet<String>> getTermAncestorMap() {
+		String ancestorQuery = "select a.term_key, a.ancestor_term_key "
+			+ "from term_ancestor a, term t " 
+			+ "where a.term_key = t.term_key " 
+			+ "and t.vocab_name = '" + vocabName + "'";
+		
+		HashMap<String, HashSet<String>> ancestorMap = makeHash(ancestorQuery, "term_key", "ancestor_term_key");
+		logger.info("Found ancestors for " + ancestorMap.size() + " terms");
+		return ancestorMap;
+	}
+
+	// returns a single value for the given 'key' from 'map' and throws an Exception if more than one value exists
+	// or if if 'key' does not exist in 'map'.
+	public String getOne (HashMap<String, HashSet<String>> map, String key) throws Exception {
+		if (!map.containsKey(key)) {
+			throw new Exception(key + " is not a key of map");
+		}
+		if (map.get(key).size() > 1) {
+			throw new Exception(map.get(key).size() + " values for key " + key + " in map");
+		}
+		for (String s : map.get(key)) {
+			return s;
+		}
+		throw new Exception(key + " has an empty set as a value");
+	}
+	
+	public void index() throws Exception
+	{
+		HashMap<String, HashSet<String>> genotypeToMarkers = getGenotypeToMarkersMap();
+		HashMap<String, HashSet<String>> annotationToRefs = getAnnotationToRefsMap();
+		HashMap<String, HashSet<String>> annotationToSeqNum = getAnnotationToSeqNumMap();
+		HashMap<String, HashSet<String>> genotypeToStrain = getGenotypeInfoMap("background_strain");
+		HashMap<String, HashSet<String>> genotypeToAlleles = getGenotypeInfoMap("combination_1");
+		HashMap<String, HashSet<String>> termKeyToTerm = getTermDataMap("term");
+		HashMap<String, HashSet<String>> termKeyToID = getTermDataMap("primary_id");
+		HashMap<String, HashSet<String>> ancestors = getTermAncestorMap();
+		
 		// Our main relationship is between annotation keys and MP term
 		// IDs, while also including the ordering by genotype and by
 		// MP term.  In order to drive the display from the Solr index
@@ -179,7 +176,7 @@ public class MPAnnotationIndexerSQL extends Indexer {
 		// each annotation from annotationToRefs.
 
 		Integer start = 0;
-		Integer end = maxAnnotKey;
+		Integer end = getMaxAnnotationKey();
 		int chunkSize = 10000;
 
 		int modValue = end.intValue() / chunkSize;
@@ -193,18 +190,20 @@ public class MPAnnotationIndexerSQL extends Indexer {
 			end = start + chunkSize;
 
 			logger.info ("Processing annotation key > " + start + " and <= " + end);
-			String mainQuery = "select t.annotation_key, t.search_term_id, t.genotype_key "
-					+ "from tmp_mp_annotations t "
-					+ "where t.annotation_key > "+start+" and t.annotation_key <= "+end;
+			String mainQuery = "select a.annotation_key, a.term_key, g.genotype_key " 
+				+ "from annotation a, genotype_to_annotation t, genotype g " 
+				+ "where a.annotation_type = '" + annotationType + "' "
+				+ "and a.annotation_key > " + start + " and a.annotation_key <= " + end + " "
+				+ "and a.annotation_key = t.annotation_key " 
+				+ "and t.genotype_key = g.genotype_key " 
+				+ "and g.combination_1 is not null";
 
 			ResultSet rs_overall = ex.executeProto(mainQuery);
-			//logger.info("Found annotation/genotype pairs");
 
 			boolean hasRecord = rs_overall.next();
 
 			// Finally we need to bundle the data into Solr documents and push
 			// them over to the Solr server for indexing.
-
 
 			logger.info("Parsing annotation/genotype pairs");
 
@@ -213,53 +212,53 @@ public class MPAnnotationIndexerSQL extends Indexer {
 				uniqueKey++;
 
 				String annotKey = rs_overall.getString("annotation_key");
-				String genotype = rs_overall.getString("genotype_key");
-				String searchTermID = rs_overall.getString("search_term_id");
+				String genotypeKey = rs_overall.getString("genotype_key");
+				String termKey = rs_overall.getString("term_key");
 
 				SolrInputDocument doc = new SolrInputDocument();
 
-				doc.addField(IndexConstants.UNIQUE_KEY,
-						Integer.toString(uniqueKey));
+				doc.addField(IndexConstants.UNIQUE_KEY, annotKey);
 				doc.addField(IndexConstants.ANNOTATION_KEY, annotKey);
-				doc.addField(IndexConstants.GENOTYPE_KEY, genotype);
-				doc.addField(IndexConstants.TERM_ID, searchTermID);
+				doc.addField(IndexConstants.GENOTYPE_KEY, genotypeKey);
+				
+				// make record searchable by this term ID and the IDs of all its ancestors
 
-				// add in pieces pulled out of mainQuery
+				if (termKeyToID.containsKey(termKey)) {
+					String id = getOne(termKeyToID, termKey);
+					doc.addField(IndexConstants.TERM_ID, id);
+					doc.addField(IndexConstants.ANNOTATED_TERM_ID, id);
+				}
 
-				if (annotationToTerm.containsKey(annotKey)) {
-					for (String term: annotationToTerm.get(annotKey)) {
-						doc.addField(IndexConstants.TERM, term);
+				if (ancestors.containsKey(termKey)) {
+					for (String ancestorKey : ancestors.get(termKey)) {
+						doc.addField(IndexConstants.TERM_ID, getOne(termKeyToID, ancestorKey));
 					}
 				}
 
-				if (annotationToTermID.containsKey(annotKey)) {
-					for (String termID: annotationToTermID.get(annotKey)) {
-						doc.addField(IndexConstants.ANNOTATED_TERM_ID, termID);
-					}
+				// annotated term for display
+				if (termKeyToTerm.containsKey(termKey)) {
+					doc.addField(IndexConstants.TERM, getOne(termKeyToTerm, termKey));
 				}
 
-				if (annotationToStrain.containsKey(annotKey)) {
-					for (String strain: annotationToStrain.get(annotKey)) {
-						doc.addField(IndexConstants.BACKGROUND_STRAIN, strain);
-					}
+				// background strain for genotype
+				if (genotypeToStrain.containsKey(genotypeKey)) {
+					doc.addField(IndexConstants.BACKGROUND_STRAIN, getOne(genotypeToStrain, genotypeKey));
 				}
 
-				if (annotationToAlleles.containsKey(annotKey)) {
-					for (String alleles: annotationToAlleles.get(annotKey)) {
-						doc.addField(IndexConstants.ALLELE_PAIRS, alleles);
-					}
+				// allele pairs for genotype
+				if (genotypeToAlleles.containsKey(genotypeKey)) {
+					doc.addField(IndexConstants.ALLELE_PAIRS, getOne(genotypeToAlleles, genotypeKey));
 				}
 
+				// sequence number
 				if (annotationToSeqNum.containsKey(annotKey)) {
-					for (String seqNum: annotationToSeqNum.get(annotKey)) {
-						doc.addField(IndexConstants.BY_GENOTYPE_TERM, seqNum);
-					}
+					doc.addField(IndexConstants.BY_GENOTYPE_TERM, getOne(annotationToSeqNum, annotKey));
 				}
 
 				// include markers for each annotation's genotype
 
-				if (genotypeToMarkers.containsKey(genotype)) {
-					for (String markerKey: genotypeToMarkers.get(genotype)) {
+				if (genotypeToMarkers.containsKey(genotypeKey)) {
+					for (String markerKey: genotypeToMarkers.get(genotypeKey)) {
 						doc.addField(IndexConstants.MRK_KEY, markerKey);
 					}
 				}
@@ -278,9 +277,6 @@ public class MPAnnotationIndexerSQL extends Indexer {
 
 				if (docs.size() > 1000) {
 					writeDocs(docs);
-					// skip logging info, as once we hit 16kb of log, process hangs when run via
-					// a Python wrapper:
-					//                    logger.info("Committed " + docs.size() + " docs to solr");
 					docs = new ArrayList<SolrInputDocument>();
 				}
 			}
@@ -289,9 +285,6 @@ public class MPAnnotationIndexerSQL extends Indexer {
 		if (docs.size() > 0) {
 			writeDocs(docs);
 			commit();
-			// skip logging info, as once we hit 16kb of log, process hangs when run via
-			// a Python wrapper:
-			//                logger.info("Committed " + docs.size() + " docs to solr");
 		}
 		logger.info("Done adding " + uniqueKey + " docs to solr; completed MP/Annotation index.");
 	}
