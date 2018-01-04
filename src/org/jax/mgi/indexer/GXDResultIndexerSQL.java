1e473afcefebd211339eeea1567e1818ffd91f61
Jonathan Beal <jsb@jax.org>
2018-01-04 12:20:07 -0500

    fixed bug in assigning allele pairs for each result's genotype

--- src/org/jax/mgi/indexer/GXDResultIndexerSQL.java
+++ src/org/jax/mgi/indexer/GXDResultIndexerSQL.java
@@ -839,7 +839,7 @@ public class GXDResultIndexerSQL extends Indexer {
 				doc.addField(GxdResultFields.JNUM, jnum);
 				doc.addField(GxdResultFields.PUBMED_ID, pubmedID.get(rs.getString("reference_key")));
 				doc.addField(GxdResultFields.SHORT_CITATION, citation.get(rs.getString("reference_key")));
-				doc.addField(GxdResultFields.GENOTYPE, allelePairs.get("genotype_key"));
+				doc.addField(GxdResultFields.GENOTYPE, allelePairs.get(rs.getString("genotype_key")));
 				doc.addField(GxdResultFields.PATTERN, rs.getString("pattern"));
 
 				// multi values
