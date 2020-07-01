3912ed68c5951ed26f05535e02cf19dba4fe2104
jsb <jsb@jax.org>
2020-07-01 14:36:13 -0400

    removed unused strainAC index

--- src/org/jax/mgi/indexer/Main.java
+++ src/org/jax/mgi/indexer/Main.java
@@ -53,7 +53,6 @@ public class Main {
 		indexerMap.put("cdna", new CdnaIndexerSQL());
 		indexerMap.put("probe", new ProbeIndexerSQL());
 		indexerMap.put("strain", new StrainIndexerSQL());
-		indexerMap.put("strainAC", new StrainAutoCompleteIndexerSQL());
 		indexerMap.put("mapping", new MappingIndexerSQL());
 		indexerMap.put("gxdHtSample", new GXDHtSampleIndexerSQL());
 		indexerMap.put("gxdHtExperiment", new GXDHtExperimentIndexerSQL());
